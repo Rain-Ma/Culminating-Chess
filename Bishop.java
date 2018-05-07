24f01148897bfe44a886d0ae20da4696bda254cf
lRangers <38949427+lRangers@users.noreply.github.com>
2018-05-07 09:39:41 -0400

    Add files via upload

--- Bishop.java
+++ Bishop.java
@@ -1,11 +1,8 @@
-public abstract class Bishop
+import java.util.*;
+public abstract class Bishop extends ChessPiece
 {
     // instance variables - replace the example below with your own
-    private int[][] moves;
-    private boolean isWhite;
-    int row;
-    int col;
-    
+
     /**
      * Constructor for objects of class asdf
      */
@@ -14,20 +11,77 @@ public abstract class Bishop
         this.row = row;
         this.col = col;
         this.isWhite = isWhite;
-        
+
     }
-    
+
     public abstract void findMove();
-    
-    
-    public int[][] getMoves()
+
+    public void newMoves(ArrayList[][] moves)
     {
-        return moves;
+        boolean ur = true;
+        boolean dr = true;
+        boolean ul = true;
+        boolean dl = true;
+        for(int r = 0; r<8; r++)
+        {           
+            for(int c = 0; c<8; c++)
+            {
+                if(r>row && c>col && ur == true)
+                {
+                    if(ChessPiece[r][col] instanceof ChessPiece){
+                        ur = false;
+                        break;
+                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                        break;
+                    else if(Math.abs(row - r) != Math.abs(col - c))
+                        break;
+                    else
+                        moves[r][c].add(r,c);
+                }
+                else if(r<row && c>col && dr == true)
+                {
+                    if(ChessPiece[r][col] instanceof ChessPiece){
+                        dr = false;
+                        break;
+                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                        break;
+                    else if(Math.abs(row - r) != Math.abs(col - c))
+                        break;
+                    else
+                        moves[r][c].add(r,c);
+                }
+                else if(r>row && c<col && ul == true)
+                {
+                    if(ChessPiece[r][col] instanceof ChessPiece){
+                        ul = false;
+                        break;
+                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                        break;
+                    else if(Math.abs(row - r) != Math.abs(col - c))
+                        break;
+                    else
+                        moves[r][c].add(r,c);
+                }
+                else if(r<row && c<col && dl == true)
+                {
+                    if(ChessPiece[r][col] instanceof ChessPiece){
+                        dl = false;
+                        break;
+                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                        break;
+                    else if(Math.abs(row - r) != Math.abs(col - c))
+                        break;
+                    else
+                        moves[r][c].add(r,c);
+                }
+            }
+        }
+        this.moves = moves;
     }
-    
+
     public boolean getIsWhite()
     {
         return isWhite;
     }
-    
+
 }
