775e0556a41d188557b680c78fe5ad71a22e763c
lRangers <38949427+lRangers@users.noreply.github.com>
2018-05-07 09:51:40 -0400

    Add files via upload

--- Knight.java
+++ Knight.java
@@ -1,11 +1,8 @@
-public abstract class Knight
+import java.util.*;
+public abstract class Knight extends ChessPiece
 {
     // instance variables - replace the example below with your own
-    private int[][] moves;
-    private boolean isWhite;
-    int row;
-    int col;
-    
+
     /**
      * Constructor for objects of class asdf
      */
@@ -14,20 +11,33 @@ public abstract class Knight
         this.row = row;
         this.col = col;
         this.isWhite = isWhite;
-        
+
     }
-    
+
     public abstract void findMove();
-    
-    
-    public int[][] getMoves()
+
+    public void newMoves(ArrayList[][] moves)
     {
-        return moves;
+        for(int r = 0; r<8; r++)
+        {
+            for(int c = 0; c<8; c++)
+            {
+                if(ChessPiece[r][c] instanceof ChessPiece && ChessPiece[row][col].isWhite != ChessPiece[r][c].iswhite)
+                    break;
+                else if(Math.abs(row - r) == 2 && Math.abs(col - c) == 1) 
+                    moves[r][c].add(r,c);
+                else if (Math.abs(row - r) == 1 && Math.abs(col - c) == 2) 
+                    moves[r][c].add(r,c);
+                else
+                    break;
+                this.moves = moves;
+            }
+        }
     }
-    
+
     public boolean getIsWhite()
     {
         return isWhite;
     }
-    
+
 }
