9bc76d62eedb497e8bb4f474e83e8963bdf90be9
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-05-09 10:10:02 -0400

    Doesn't print pieces yet
    calls frame before new array

--- ChessGUI.java
+++ ChessGUI.java
@@ -5,10 +5,10 @@
  * @author Justin Chu
  * @version May 3rd, 2018
  */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.*;
 public class ChessGUI
 {
     public static void main (String [] args)
@@ -30,6 +30,7 @@ class GameFrame extends JFrame implements ActionListener
 {
     //We'll make our JPanel an instance variable so we can use it in multiple methods
     private JPanel pane;
+    ChessPiece[][] images = new ChessPiece[8][8];
 
     public GameFrame()
     {
@@ -39,16 +40,36 @@ class GameFrame extends JFrame implements ActionListener
         //We are in the frame (ColourFrame) whose Content Pane that we want, we can't say frame.getContentPane()
         pane = (JPanel)getContentPane();
         pane.setLayout(new GridLayout(8,8));
-        
+
         JButton[][] buttons = new JButton[8][8];
-        
-        for(int r = 8; r > 0; r--)
+        for(int r = 0; r < 8; r++)
         {
             for(int c = 0; c < 8; c++)
             {
-                String loc = Integer.toString(r) + ", " + Integer.toString(c+1);
-                buttons[r][c] = new JButton(loc);
-                buttons[r][c].setBackground(Color.BLACK);
+                //String loc = Integer.toString(r) + ", " + Integer.toString(c+1);
+                if(images[r][c]==null)
+                {
+                    buttons[r][c] = new JButton();
+                    System.out.println("ok");    
+                }
+                else
+                {
+                    buttons[r][c] = new JButton(new ImageIcon("ChessPieceIcons/BlackPawn.png"));
+                    System.out.println("no");    
+                }
+                if(r % 2 != 0&& (c+1)%2 != 0)
+                {
+                    buttons[r][c].setBackground(Color.BLACK);
+                    buttons[r][c].setForeground(buttons[r][c].getBackground());
+                }
+                else if(r % 2 == 0&& (c+1)%2 == 0)
+                {
+                    buttons[r][c].setBackground(Color.BLACK);
+                }
+                else
+                {
+                    buttons[r][c].setBackground(Color.WHITE);
+                }
                 pane.add(buttons[r][c]);
             }
         }
@@ -56,6 +77,18 @@ class GameFrame extends JFrame implements ActionListener
         //Frame here is implicit
         setSize(1000,1000);
         setVisible(true);
+    }
+
+    public void getBoard(ChessPiece[][] board)
+    {
+        for(int r = 0; r < 8; r++)
+        {
+            for(int c = 0; c < 8; c++)
+            {
+                images[r][c] = board[r][c];
+                System.out.println(images[r][c]);
+            }
+        }
     }
 
     /**
