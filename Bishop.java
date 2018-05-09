c823d7b85326f7d9063ebb6bd55d4da58fb5be22
lRangers <38949427+lRangers@users.noreply.github.com>
2018-05-09 09:18:32 -0400

    Add files via upload

--- Bishop.java
+++ Bishop.java
@@ -8,80 +8,81 @@ public abstract class Bishop extends ChessPiece
      */
     public Bishop(int row, int col, boolean isWhite)
     {
-        this.row = row;
-        this.col = col;
-        this.isWhite = isWhite;
-
+        super(row,col,isWhite);
     }
 
     public abstract void findMove();
-
-    public void newMoves(ArrayList[][] moves)
+    
+    public void newMoves(ChessPiece[][] moves)
     {
         boolean ur = true;
         boolean dr = true;
         boolean ul = true;
         boolean dl = true;
+        int[][] move = new int[8][2];
+        int counter = 0;
         for(int r = 0; r<8; r++)
         {           
             for(int c = 0; c<8; c++)
             {
-                if(r>row && c>col && ur == true)
+                if(r>getRow() && c>getCol() && ur == true)
                 {
-                    if(ChessPiece[r][col] instanceof ChessPiece){
+                    if(moves[r][getCol()] instanceof ChessPiece){
                         ur = false;
                         break;
-                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                    }else if(getRow() == r && getCol() == c) // makes sure the piece is actually moved
                         break;
-                    else if(Math.abs(row - r) != Math.abs(col - c))
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
                         break;
-                    else
-                        moves[r][c].add(r,c);
+                    else{
+                        move[counter][0] = r;
+                        move[counter][1] = c;
+                    }
                 }
-                else if(r<row && c>col && dr == true)
+                else if(r<getRow() && c>getCol() && dr == true)
                 {
-                    if(ChessPiece[r][col] instanceof ChessPiece){
+                    if(moves[r][getCol()] instanceof ChessPiece){
                         dr = false;
                         break;
-                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                    }else if(getRow() == r && getCol() == c) // makes sure the piece is actually moved
                         break;
-                    else if(Math.abs(row - r) != Math.abs(col - c))
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
                         break;
-                    else
-                        moves[r][c].add(r,c);
+                    else{
+                        move[counter][0] = r;
+                        move[counter][1] = c;
+                    }
                 }
-                else if(r>row && c<col && ul == true)
+                else if(r>getRow() && c<getCol() && ul == true)
                 {
-                    if(ChessPiece[r][col] instanceof ChessPiece){
+                    if(moves[r][getCol()] instanceof ChessPiece){
                         ul = false;
                         break;
-                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                    }else if(getRow() == r && getCol() == c) // makes sure the piece is actually moved
                         break;
-                    else if(Math.abs(row - r) != Math.abs(col - c))
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
                         break;
-                    else
-                        moves[r][c].add(r,c);
+                    else{
+                        move[counter][0] = r;
+                        move[counter][1] = c;
+                    }
                 }
-                else if(r<row && c<col && dl == true)
+                else if(r<getRow() && c<getCol() && dl == true)
                 {
-                    if(ChessPiece[r][col] instanceof ChessPiece){
+                    if(moves[r][getCol()] instanceof ChessPiece){
                         dl = false;
                         break;
-                    }else if(row == r && col == c) // makes sure the piece is actually moved
+                    }else if(getRow() == r && getCol() == c) // makes sure the piece is actually moved
                         break;
-                    else if(Math.abs(row - r) != Math.abs(col - c))
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
                         break;
-                    else
-                        moves[r][c].add(r,c);
+                    else{
+                        move[counter][0] = r;
+                        move[counter][1] = c;
+                    }
                 }
+                counter++;
             }
         }
-        this.moves = moves;
     }
-
-    public boolean getIsWhite()
-    {
-        return isWhite;
-    }
-
 }
