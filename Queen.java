c36ca10a15365296fc09bca2e1acf8c5db6fe021
lRangers <38949427+lRangers@users.noreply.github.com>
2018-05-09 10:09:09 -0400

    Add files via upload

--- Queen.java
+++ Queen.java
@@ -19,74 +19,16 @@ public abstract class Queen extends ChessPiece
         boolean dl = true;
         for(int r = 0; r<8; r++)
         {
-            if(moves[r][getCol()] instanceof ChessPiece != true)
+            for(int c = 0; c<8; c++)
             {
-                for(int c = 0; c<8; c++)
+                if(r>getRow() && c>getCol() && ur == true)
                 {
-                    if(r>getRow() && c>getCol() && ur == true)
-                    {
-                        if(moves[r][getCol()] instanceof ChessPiece){
-                            ur = false;
-                            break;
-                        }else if(getRow() == r && getCol() == c) 
-                            break;
-                        else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
-                            break;
-                        else{
-                            move[counter][0] = r;
-                            move[counter][1] = c;
-                            counter++;
-                        }
-                    }
-                    else if(r<getRow() && c>getCol() && dr == true)
-                    {
-                        if(moves[r][getCol()] instanceof ChessPiece){
-                            dr = false;
-                            break;
-                        }else if(getRow() == r && getCol() == c) 
-                            break;
-                        else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
-                            break;
-                        else{
-                            move[counter][0] = r;
-                            move[counter][1] = c;
-                            counter++;
-                        }
-                    }
-                    else if(r>getRow() && c<getCol() && ul == true)
-                    {
-                        if(moves[r][getCol()] instanceof ChessPiece){
-                            ul = false;
-                            break;
-                        }else if(getRow() == r && getCol() == c) 
-                            break;
-                        else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
-                            break;
-                        else{
-                            move[counter][0] = r;
-                            move[counter][1] = c;
-                            counter++;
-                        }
-                    }
-                    else if(r<getRow() && c<getCol() && dl == true)
-                    {
-                        if(moves[r][getCol()] instanceof ChessPiece){
-                            dl = false;
-                            break;
-                        }else if(getRow() == r && getCol() == c) 
-                            break;
-                        else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
-                            break;
-                        else{
-                            move[counter][0] = r;
-                            move[counter][1] = c;
-                            counter++;
-                        }
-                    }else if(getRow() != r && getCol() != c) 
+                    if(moves[r][getCol()] instanceof ChessPiece){
+                        ur = false;
                         break;
-                    else if(getRow() == r && getCol() == c) 
+                    }else if(getRow() == r && getCol() == c) 
                         break;
-                    else if(moves[r][c] instanceof ChessPiece)
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
                         break;
                     else{
                         move[counter][0] = r;
@@ -94,12 +36,69 @@ public abstract class Queen extends ChessPiece
                         counter++;
                     }
                 }
+                else if(r<getRow() && c>getCol() && dr == true)
+                {
+                    if(moves[r][getCol()] instanceof ChessPiece){
+                        dr = false;
+                        break;
+                    }else if(getRow() == r && getCol() == c) 
+                        break;
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
+                        break;
+                    else{
+                        move[counter][0] = r;
+                        move[counter][1] = c;
+                        counter++;
+                    }
+                }
+                else if(r>getRow() && c<getCol() && ul == true)
+                {
+                    if(moves[r][getCol()] instanceof ChessPiece){
+                        ul = false;
+                        break;
+                    }else if(getRow() == r && getCol() == c) 
+                        break;
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
+                        break;
+                    else{
+                        move[counter][0] = r;
+                        move[counter][1] = c;
+                        counter++;
+                    }
+                }
+                else if(r<getRow() && c<getCol() && dl == true)
+                {
+                    if(moves[r][getCol()] instanceof ChessPiece){
+                        dl = false;
+                        break;
+                    }else if(getRow() == r && getCol() == c) 
+                        break;
+                    else if(Math.abs(getRow() - r) != Math.abs(getCol() - c))
+                        break;
+                    else{
+                        move[counter][0] = r;
+                        move[counter][1] = c;
+                        counter++;
+                    }
+                }else if(moves[r][getCol()] instanceof ChessPiece){
+                    break;
+                }else if(getRow() != r && getCol() != c) 
+                    break;
+                else if(getRow() == r && getCol() == c) 
+                    break;
+                else if(moves[r][c] instanceof ChessPiece)
+                    break;
+                else{
+                    move[counter][0] = r;
+                    move[counter][1] = c;
+                    counter++;
+                }
             }
             if(counter>27)
                 break;
         }
     }
-
+    
     public void newMoves(ChessPiece[][] moves)
     {
 
