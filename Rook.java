0174b73c89bfaa800b58307dc79c1280c27f8c36
Rain-Ma <38288737+Rain-Ma@users.noreply.github.com>
2018-05-09 22:18:58 -0400

    Add files via upload

--- Rook.java
+++ Rook.java
@@ -13,15 +13,13 @@ public abstract class Rook extends ChessPiece
      */
     public Rook(int row, int col, boolean isWhite)
     {
-        this.row = row;
-        this.col = col;
-        this.isWhite = isWhite;
+        super(row,col,isWhite);
         moved = false;
     }
 
     public void findMove(ChessPiece[][] board)
     {
-        int[][] moves = new int[14][2]
+        int[][] moves = new int[14][2];
         int count = 0;
         for( int r = getRow()+1;r<8;r++)
         {
@@ -38,15 +36,13 @@ public abstract class Rook extends ChessPiece
                 {
                     break;
                 }
-             
-             }
-             moves[count][0] = r;
-             moves[count][1] = getCol();
-             count++;
-            
-            
-         }
-         for( int r = getRow()-1;r>=0;r--)
+
+            }
+            moves[count][0] = r;
+            moves[count][1] = getCol();
+            count++;           
+        }
+        for( int r = getRow()-1;r>=0;r--)
         {
             if(board[r][getCol()] instanceof ChessPiece)
             {
@@ -61,15 +57,13 @@ public abstract class Rook extends ChessPiece
                 {
                     break;
                 }
-             
-             }
-             moves[count][0] = r;
-             moves[count][1] = getCol();
-             count++;
-            
-            
-         }
-         for( int c = getCol()+1;c<8;c++)
+
+            }
+            moves[count][0] = r;
+            moves[count][1] = getCol();
+            count++;       
+        }
+        for( int c = getCol()+1;c<8;c++)
         {
             if(board[getRow()][c] instanceof ChessPiece)
             {
@@ -84,22 +78,20 @@ public abstract class Rook extends ChessPiece
                 {
                     break;
                 }
-             
-             }
-             moves[count][0] = getRow();
-             moves[count][1] = c;
-             count++;
-            
-            
-         }
-         for( int c = getRow()-1;r<8;r++)
+
+            }
+            moves[count][0] = getRow();
+            moves[count][1] = c;
+            count++;   
+        }
+        for( int c = getRow()-1;c<8;c++)
         {
-            if(board[r][getCol()] instanceof ChessPiece)
+            if(board[getRow()][c] instanceof ChessPiece)
             {
-                if(board[r][getCol()].getIsWhite()!=getIsWhite())
+                if(board[getRow()][c].getIsWhite()!=getIsWhite())
                 {
-                    moves[count][0] = r;
-                    moves[count][1] = getCol();
+                    moves[count][0] = getRow();
+                    moves[count][1] = c;
                     count++;
                     break;
                 }
@@ -107,25 +99,30 @@ public abstract class Rook extends ChessPiece
                 {
                     break;
                 }
-             
-             }
-             moves[count][0] = r;
-             moves[count][1] = getCol();
-             count++;
-            
-            
-         }
-            
-        
-        
-        
+            }
+            moves[count][0] = getRow();
+            moves[count][1] = c;
+            count++;   
+        }
+        int[][] finaleMoves = new int[count][2];
+        for(int i =0; i <finaleMoves.length;i++)
+        {
+            for(int j=0;j<2;j++)
+            { 
+                finaleMoves[i][j] = moves[i][j];
+            }
+        }
+        newMoves(finaleMoves);
         
     }
-    
+
     public boolean hasMoved()
     {
         return moved;
     }
-    
+
     public String toString()
+    {
+        return "1";
+    }
 }
