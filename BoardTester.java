1d7d26b7f9ead1e985819022895b53b8cbc611c4
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-05-13 16:15:18 -0400

    Create BoardTester.java

---
+++ BoardTester.java
@@ -0,0 +1,15 @@
+
+/**
+ * Tests the Board class
+ *
+ * @author Justin Chu
+ * @version May 13th, 2018
+ */
+public class BoardTester
+{
+    public static void main (String[] args)
+    {
+        Board board = new Board();
+        board.startGame();        
+    }
+}
