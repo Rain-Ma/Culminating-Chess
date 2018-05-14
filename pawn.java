ad9a108c252e4f45b44eb66b8ad6f80bab0fbe91
Rain-Ma <38288737+Rain-Ma@users.noreply.github.com>
2018-05-14 19:04:29 -0400

    Add files via upload

---
+++ pawn.java
@@ -0,0 +1,135 @@
+
+/**
+ * Write a description of class pawn here.
+ *
+ * @author Rain Ma
+ * @version 09/05/2018
+ */
+public class pawn extends ChessPiece
+{
+    boolean moved;
+    boolean enPassant;
+    public pawn(int row,int col,boolean isWhite)
+    {
+        super(row,col,isWhite);
+        moved = false;
+        enPassant = false;
+    }
+
+    public void findMove(ChessPiece[][] board)
+    {
+        int[][] moves =  new int[4][2];
+        int count=0;
+        if(getIsWhite())
+        {
+
+            if(board[getRow()+1][getCol()]==null)
+            {
+                moves[count][0] = getRow()+1;
+                moves[count][1] = getCol();
+                count++;
+                if(!moved&&board[getRow()+2][getCol()]==null)
+                {
+                    moves[count][0] = getRow()+2;
+                    moves[count][1] = getCol();
+                    count++;
+                }
+            }
+            if(getCol()>0)
+            {
+                if(board[getRow()+1][getCol()+1]!=null)
+                {
+                    if(board[getRow()+1][getCol()+1].getIsWhite()==getIsWhite())
+                    {
+                        moves[count][0] = getRow()+1;
+                        moves[count][1] = getCol()+1;
+                        count++;
+                    }
+                }
+
+            }
+            if(getCol()<7)
+            {
+                if(board[getRow()+1][getCol()-1]!=null)
+                {
+                    if(board[getRow()+1][getCol()-1].getIsWhite()==getIsWhite())
+                    {
+                        moves[count][0] = getRow()+1;
+                        moves[count][1] = getCol()-1;
+                        count++;
+                    }
+                }
+
+            }
+        }
+        else
+        {
+             if(board[getRow()-1][getCol()]==null)
+            {
+                moves[count][0] = getRow()-1;
+                moves[count][1] = getCol();
+                count++;
+                if(!moved&&board[getRow()-2][getCol()]==null)
+                {
+                    moves[count][0] = getRow()- -2;
+                    moves[count][1] = getCol();
+                    count++;
+                }
+            }
+            if(getCol()>0)
+            {
+                if(board[getRow()+1][getCol()+1]!=null)
+                {
+                    if(board[getRow()+1][getCol()+1].getIsWhite()==getIsWhite())
+                    {
+                        moves[count][0] = getRow()+1;
+                        moves[count][1] = getCol()+1;
+                        count++;
+                    }
+                }
+
+            }
+            if(getCol()<7)
+            {
+                if(board[getRow()+1][getCol()-1]!=null)
+                {
+                    if(board[getRow()+1][getCol()-1].getIsWhite()==getIsWhite())
+                    {
+                        moves[count][0] = getRow()+1;
+                        moves[count][1] = getCol()-1;
+                        count++;
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    public double evaluate()
+    {
+
+        return 2;
+    }
+
+    public boolean hasMoved()
+    {
+        return moved;
+    }
+
+    public boolean canEnPassant()
+    {
+        return enPassant;
+    }
+
+    public void setMoved(boolean moved)
+    {
+        this.moved = moved;
+    }
+
+    public void setEnPassant(boolean enPassant)
+    {
+        this.enPassant = enPassant;
+    }
+
+}
