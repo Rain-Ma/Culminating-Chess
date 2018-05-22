4c45bc8d9ba27b9894119726a22cd379b22624bb
Rain-Ma <38288737+Rain-Ma@users.noreply.github.com>
2018-05-22 10:01:21 -0400

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -63,46 +63,52 @@ public class Rook extends ChessPiece
             moves[count][1] = getCol();
             count++;       
         }
-        for( int c = getCol()+1;c<8;c++)
+        if(getCol()==7)
         {
-            if(board[getRow()][c] instanceof ChessPiece)
+            for( int c = getCol()+1;c<8;c++)
             {
-                if(board[getRow()][c].getIsWhite()!=getIsWhite())
+                if(board[getRow()][c] instanceof ChessPiece)
                 {
-                    moves[count][0] = getRow();
-                    moves[count][1] = c;
-                    count++;
-                    break;
-                }
-                else
-                {
-                    break;
-                }
+                    if(board[getRow()][c].getIsWhite()!=getIsWhite())
+                    {
+                        moves[count][0] = getRow();
+                        moves[count][1] = c;
+                        count++;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
 
+                }
+                moves[count][0] = getRow();
+                moves[count][1] = c;
+                count++;   
             }
-            moves[count][0] = getRow();
-            moves[count][1] = c;
-            count++;   
         }
-        for( int c = getRow()-1;c<8;c++)
+        if(getCol()<0)
         {
-            if(board[getRow()][c] instanceof ChessPiece)
+            for( int c = getRow()-1;c<8;c++)
             {
-                if(board[getRow()][c].getIsWhite()!=getIsWhite())
+                if(board[getRow()][c] instanceof ChessPiece)
                 {
-                    moves[count][0] = getRow();
-                    moves[count][1] = c;
-                    count++;
-                    break;
+                    if(board[getRow()][c].getIsWhite()!=getIsWhite())
+                    {
+                        moves[count][0] = getRow();
+                        moves[count][1] = c;
+                        count++;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
-                else
-                {
-                    break;
-                }
+                moves[count][0] = getRow();
+                moves[count][1] = c;
+                count++;   
             }
-            moves[count][0] = getRow();
-            moves[count][1] = c;
-            count++;   
         }
         int[][] finaleMoves = new int[count][2];
         for(int i =0; i <finaleMoves.length;i++)
