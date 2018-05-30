b980ab35fe9be283cb95723f8b303b58f768dd27
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-05-29 22:49:12 -0400

    COMPLETED

--- Queen.java
+++ Queen.java
@@ -17,7 +17,7 @@ public class Queen extends ChessPiece
         super(row,col,isWhite);
     }
 
-     /**
+    /**
      * a Constructor that clones another Queen
      * 
      * @param piece This is the Queen that is being cloned
@@ -26,7 +26,7 @@ public class Queen extends ChessPiece
     {
         super(piece);
     }
-    
+
     @Override
     /**
      * @param board This is the chess board
@@ -41,233 +41,226 @@ public class Queen extends ChessPiece
         {
             for(int i = 1; i < 7; i++)
             {                   
-                if(getRow()+i > 7 || getCol()+i > 7)
+                if(getRow()+i > 7 || getCol()+i > 7) //if the loop moves off the board
                     break;
-                if(board[getRow()+i][getCol()+i] instanceof ChessPiece)
+                if(board[getRow()+i][getCol()+i] instanceof ChessPiece)// if the queen hits a chess piece
                 {
-                    if(board[getRow()+i][getCol()+i].getIsWhite() != getIsWhite())
+                    if(board[getRow()+i][getCol()+i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()+i;
                         moves[count][1] = getCol()+i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else //if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()+i;
                 moves[count][1] = getCol()+i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
-        
-        //starts diagonally checking squares from the queen to the top left corner of the board
+
+        //starts checking squares from the queen to top left corner of the board
         if(getRow() < 7 && getCol() > 0)
         {
             for(int i = 1; i < 7; i++)
             {                   
-                if(getRow()+i > 7 || getCol()-i < 0)
+                if(getRow()+i > 7 || getCol()-i < 0) //if the loop moves off the board
                     break;
-                if(board[getRow()+i][getCol()-i] instanceof ChessPiece)
+                if(board[getRow()+i][getCol()-i] instanceof ChessPiece) // if the queen hits a chess piece
                 {
-                    if(board[getRow()+i][getCol()-i].getIsWhite() != getIsWhite())
+                    if(board[getRow()+i][getCol()-i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()+i;
                         moves[count][1] = getCol()-i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else//if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()+i;
                 moves[count][1] = getCol()-i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
 
-        //starts diagonally checking squares from the queen to the bottom right corner of the board
+        //starts checking squares from the queen to bottom right corner of the board
         if(getRow() > 0 && getCol() < 7)
         {
             for(int i = 1; i < 7; i++)
             {                   
-                if(getRow()-i < 0 || getCol()+i > 7)
+                if(getRow()-i < 0 || getCol()+i > 7) //if the loop moves off the board
                     break;
-                if(board[getRow()-i][getCol()+i] instanceof ChessPiece)
+                if(board[getRow()-i][getCol()+i] instanceof ChessPiece)// if the queen hits a chess piece
                 {
-                    if(board[getRow()-i][getCol()+i].getIsWhite() != getIsWhite())
+                    if(board[getRow()-i][getCol()+i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()-i;
                         moves[count][1] = getCol()+i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else//if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()-i;
                 moves[count][1] = getCol()+i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
-        //starts diagonally checking squares from the queen  to the bottom left corner of the board
+        //starts checking squares from the queen to bottem left corner of the board
         if(getRow() > 0 && getCol() > 0)
         {
             for(int i = 1; i < 7; i++)
             {                   
-                if(getRow()-i < 0 || getCol()-i < 0)
+                if(getRow()-i < 0 || getCol()-i < 0)//if the loop moves off the board
                     break;
-                if(board[getRow()-i][getCol()-i] instanceof ChessPiece)
+                if(board[getRow()-i][getCol()-i] instanceof ChessPiece)// if the queen hits a chess piece
                 {
-                    if(board[getRow()-i][getCol()-i].getIsWhite() != getIsWhite())
+                    if(board[getRow()-i][getCol()-i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()-i;
                         moves[count][1] = getCol()-i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else//if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()-i;
                 moves[count][1] = getCol()-i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
-        
-        //starts checking squares linearly from the queen to the top of the board
+
         if(getRow()<7)
         {
             for(int r = getRow()+1;r<8;r++)
             {
-                if(board[r][getCol()] instanceof ChessPiece)
+                if(board[r][getCol()] instanceof ChessPiece) //if there is a piece on that square
                 {
-                    if(board[r][getCol()].getIsWhite()!=getIsWhite())
+                    if(board[r][getCol()].getIsWhite()!=getIsWhite()) //if it is an enemy piece
                     {
+                        //add coordinates to array
                         moves[count][0] = r;
                         moves[count][1] = getCol();
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else //if it is friendly piece
                     {
                         break;
                     }
-                }         
+                }    
+                //add coordinates to array
                 moves[count][0] = r;
                 moves[count][1] = getCol();
-                count++; 
+                count++; //increase number of possible moves in array
             }
         }
-
-        //starts checking squares linearly from the queen to the bottom of the board
+        //if the queen trys to move up the board
         if(getRow()>0)
         {
-            for( int r = getRow()-1;r>=0;r--)
+            for(int r = getRow()-1;r>=0;r--)
             {
-                if(board[r][getCol()] instanceof ChessPiece)
+                if(board[r][getCol()] instanceof ChessPiece) //if there is a piece on that square
                 {
-                    if(board[r][getCol()].getIsWhite()!=getIsWhite())
+                    if(board[r][getCol()].getIsWhite()!=getIsWhite()) //if it is an enemy piece
                     {
+                        //add coordinates to array
                         moves[count][0] = r;
                         moves[count][1] = getCol();
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else //if it is a friendly piece
                     {
                         break;
                     }
-
                 }
+                //add coordinates to array
                 moves[count][0] = r;
                 moves[count][1] = getCol();
-                count++;       
+                count++; //increase number of possible moves in array
             }
         }
-
-        //starts checking squares linearly from the queen to the right of the board
+        //if the queen trys to move right
         if(getCol()<7)
         {
             for( int c = getCol()+1;c<8;c++)
             {
-                if(board[getRow()][c] instanceof ChessPiece)
+                if(board[getRow()][c] instanceof ChessPiece) //if there is a piece on that square
                 {
-                    if(board[getRow()][c].getIsWhite()!=getIsWhite())
+                    if(board[getRow()][c].getIsWhite()!=getIsWhite())//if it is an enemy piece
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow();
                         moves[count][1] = c;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else //if it is a friendly piece
                     {
                         break;
                     }
-
                 }
+                //add coordinates to array
                 moves[count][0] = getRow();
                 moves[count][1] = c;
-                count++;   
+                count++; //increase number of possible moves in array
             }
         }
-
-        //starts checking squares linearly from the queen to the left of the board
+        //if the queen trys to move left
         if(getCol()>0)
         {
             for( int c = getCol()-1;c>=0;c--)
             {
-                if(board[getRow()][c] instanceof ChessPiece)
+                if(board[getRow()][c] instanceof ChessPiece) //if there is a piece on that square
                 {
-                    if(board[getRow()][c].getIsWhite()!=getIsWhite())
+                    if(board[getRow()][c].getIsWhite()!=getIsWhite()) //if it is an enemy piece
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow();
                         moves[count][1] = c;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else //if it is a freindly piece
                     {
                         break;
                     }
                 }
+                //add coordinates to array
                 moves[count][0] = getRow();
                 moves[count][1] = c;
-                count++;   
+                count++; //increase number of possible moves in array
             }
         }
-        int[][] finaleMoves = Arrays.copyOf(moves,count);
-        
-        newMoves(finaleMoves);
-    }
-    
-    @Override
-    /**
-     * @param the chessboard because the value of a piece depend on the position of other pieces.
-     * @return the value of the chessPiece
-     */
-    public double evaluate(ChessPiece[][] board)
-    {
-        double value = 9;
-        if(getIsWhite())
-        {
-            return value;
-        }
-        else
-        {
-            return value*-1;
-        }
+        int[][] finaleMoves = Arrays.copyOf(moves,count); //truncate array of possible moves
+        newMoves(finaleMoves); //make this the new array of moves the queen can make
     }
 
     @Override
     /**
-     * @return the memory adress of the queen png
+     * Returns the location of the queen icon
+     * 
+     * @return the String adress of the queen image
      */
     public String toString()
     {
-        if(getIsWhite())
+        if(getIsWhite()) //if queen is white
             return "ChessPieceIcons/WhiteQueen.png";
-        else 
+        else //if queen is black
             return "ChessPieceIcons/BlackQueen.png";
     }
 }
