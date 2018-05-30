e5eceba76f3f073eafffc1c681eb04e5394230c3
justin-chu <30902118+justin-chu@users.noreply.github.com>
2018-05-29 22:07:01 -0400

    COMPLETED

--- Bishop.java
+++ Bishop.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 /**
+ * Bishop chess piece class
  * 
  * @author Jutin Chu
  * @version May 25th, 2018
@@ -7,7 +8,8 @@ import java.util.Arrays;
 public class Bishop extends ChessPiece
 {
     /**
-     * Constructor for objects of class Rook
+     * Constructor for objects of class Bishop
+     * 
      * @param row This is the row num of the square that the Bishop is starting on
      * @param col This is the column num of the square that the Bishop is starting on
      * @param isWhite This is the color of the Bishop. if isWhite is true, the Bishop is white
@@ -16,7 +18,7 @@ public class Bishop extends ChessPiece
     {
         super(row,col,isWhite);
     }
-    
+
     /**
      * a Constructor that clones another Bishop
      * 
@@ -26,9 +28,11 @@ public class Bishop extends ChessPiece
     {
         super(piece);
     }
-    
+
     @Override
     /**
+     * Finds all possible moves for bishop objects
+     * 
      * @param board This is the chess board
      * @param attack Not appicable to Bishop since moving and attacking is the thing 
      */
@@ -36,7 +40,7 @@ public class Bishop extends ChessPiece
     {
         int[][] moves = new int[13][2]; //Array saves all moves possible for the piece [most moves possible][coordinates]
         int count = 0; //counter for possible moves
-        
+
         //starts checking squares from the bishop to top right corner of the board
         if(getRow() < 7 && getCol() < 7)
         {
@@ -46,69 +50,75 @@ public class Bishop extends ChessPiece
                     break;
                 if(board[getRow()+i][getCol()+i] instanceof ChessPiece)// if the bishop hits a chess piece
                 {
-                    if(board[getRow()+i][getCol()+i].getIsWhite() != getIsWhite())// if the chesspiece is of the opposite color, then it can be captured
+                    if(board[getRow()+i][getCol()+i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()+i;
                         moves[count][1] = getCol()+i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else //if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()+i;
                 moves[count][1] = getCol()+i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
-        
+
         //starts checking squares from the bishop to top left corner of the board
         if(getRow() < 7 && getCol() > 0)
         {
             for(int i = 1; i < 7; i++)
             {                   
-                if(getRow()+i > 7 || getCol()-i < 0)
+                if(getRow()+i > 7 || getCol()-i < 0) //if the loop moves off the board
                     break;
-                if(board[getRow()+i][getCol()-i] instanceof ChessPiece)
+                if(board[getRow()+i][getCol()-i] instanceof ChessPiece) // if the bishop hits a chess piece
                 {
-                    if(board[getRow()+i][getCol()-i].getIsWhite() != getIsWhite())
+                    if(board[getRow()+i][getCol()-i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()+i;
                         moves[count][1] = getCol()-i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else//if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()+i;
                 moves[count][1] = getCol()-i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
-        
+
         //starts checking squares from the bishop to bottom right corner of the board
         if(getRow() > 0 && getCol() < 7)
         {
             for(int i = 1; i < 7; i++)
             {                   
-                if(getRow()-i < 0 || getCol()+i > 7)
+                if(getRow()-i < 0 || getCol()+i > 7) //if the loop moves off the board
                     break;
-                if(board[getRow()-i][getCol()+i] instanceof ChessPiece)
+                if(board[getRow()-i][getCol()+i] instanceof ChessPiece)// if the bishop hits a chess piece
                 {
-                    if(board[getRow()-i][getCol()+i].getIsWhite() != getIsWhite())
+                    if(board[getRow()-i][getCol()+i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()-i;
                         moves[count][1] = getCol()+i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else//if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()-i;
                 moves[count][1] = getCol()+i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
         //starts checking squares from the bishop to bottem left corner of the board
@@ -116,52 +126,37 @@ public class Bishop extends ChessPiece
         {
             for(int i = 1; i < 7; i++)
             {                   
-                if(getRow()-i < 0 || getCol()-i < 0)
+                if(getRow()-i < 0 || getCol()-i < 0)//if the loop moves off the board
                     break;
-                if(board[getRow()-i][getCol()-i] instanceof ChessPiece)
+                if(board[getRow()-i][getCol()-i] instanceof ChessPiece)// if the bishop hits a chess piece
                 {
-                    if(board[getRow()-i][getCol()-i].getIsWhite() != getIsWhite())
+                    if(board[getRow()-i][getCol()-i].getIsWhite() != getIsWhite())//if the chesspiece is of the opposite color, then it can be captured
                     {
+                        //add coordinates to array
                         moves[count][0] = getRow()-i;
                         moves[count][1] = getCol()-i;
-                        count++;
+                        count++; //increase number of possible moves in array
                         break;
                     }
-                    else
+                    else//if it is a friendly chess piece
                         break;
                 }
+                //add coordinates to array
                 moves[count][0] = getRow()-i;
                 moves[count][1] = getCol()-i;
-                count++;
+                count++; //increase number of possible moves in array
             }
         }
-        
-        //trims size of move array to only have the number of possible moves
-        int[][] finaleMoves = Arrays.copyOf(moves,count);
-        newMoves(finaleMoves);
-    } 
+
+        int[][] finaleMoves = Arrays.copyOf(moves,count); //truncates size of move array to only have the number of possible moves
+        newMoves(finaleMoves); //make the array the new array of moves the bishop can make
+    }
 
     @Override
     /**
-     * @param the chessboard because the value of a piece depend on the position of other pieces.
-     * @return the value of the chessPiece
-     */
-    public double evaluate(ChessPiece[][] board)
-    {
-        double value = 3.1;
-        if(getIsWhite())
-        {
-            return value;
-        }
-        else
-        {
-            return value*-1;
-        }
-    }
-    
-    @Override
-    /**
-     * @return the memory adress of the bishop png
+     * Returns String location of bishop icon
+     * 
+     * @return the String address of the bishop image
      */
     public String toString()
     {
